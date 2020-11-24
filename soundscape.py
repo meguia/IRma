7e81adfcdeea79f5ea7e54be1e954035d98dd41a
meguia <eguiamc@gmail.com>
2020-11-24 20:14:38 -0300

    soundscape update all numpy!

--- soundscape.py
+++ soundscape.py
@@ -56,29 +56,25 @@ def gini(values,ax=0):
     Compute the Gini index of values.
     """
     values.sort(axis=ax)
-    n = values.shape[0]
-    G = np.sum([i*j for i,j in zip(values,range(1,n+1))])
-    #G = 2 * G / np.sum(y) - (n+1)
+    n = values.shape[ax]
+    G = np.arange(1, n+1).reshape(-1,1)*np.ones_like(values)
+    G = 2*G/np.sum(values,axis=ax) - (n+1)
     return G/n
 
-def acoustic_evenness(spec, tstep, max_freq=10000, db_threshold = -50, freq_step=1000):
+def acoustic_diversity_even(spec, tstep, max_freq=10000, db_threshold = -50, freq_step=1000):
     """
-    Compute the Acoustic Evenness Index AEI
+    Compute the Acoustic Evenness Index AEI and Acoustic Diversity Index ADI
     """
     subspec,t = subspecs(spec,tstep)
     bands_Hz = range(0, max_freq, freq_step)
     bands_bin = [f / spec['df'] for f in bands_Hz]
     spec_AEI = 20*np.log10(subspec/np.amax(subspec,axis=(-2,-1))[...,np.newaxis,np.newaxis])
     spec_AEI_bands = np.array([spec_AEI[:,:,bb:bb+bands_bin[1],:] for bb in bands_bin])
-    values = np.average(spec_AEI_bands>db_threshold,axis=(-2,-1))
-    AEI = np.zeros((spec['nchan'],nts))
-    for n in np.arange(spec['nchan']):
-        #subspecs = [np.array(spec['s'][n,:,t0:t0+tstep]) for t0 in ts]
-        spec_AEI = np.array([20*np.log10(s/np.max(s)) for s in subspecs])
-        spec_AEI_bands = [spec_AEI[:,int(bands_bin[k]):int(bands_bin[k]+bands_bin[1]),] for k in range(len(bands_bin))]
-        values = [np.array([np.sum(spec_AEI_bands[k][t0,:,:]>db_threshold)/float(spec_AEI_bands[k][t0,:,:].size) for k in range(len(bands_bin))]) for t0 in range(nts)]
-        AEI[n,:] = [gini(values[n]) for n in range(nts)]
+    val = np.average(spec_AEI_bands>db_threshold,axis=(-2,-1)).swapaxes(0,1)
+    AEI = gini(val,ax=1)
+    tol = 1e-8
+    val[val<tol]=tol
+    ADI = np.sum(-val/np.sum(val,axis=(0,1))*np.log(val/np.sum(val,axis=(0,1))),axis=1)
+    return t, AEI, ADI       
 
-    return t, AEI       
 
-def acoustic_diversity(spec, tstep, max_freq=10000, db_threshold = -50, freq_step=1000):
