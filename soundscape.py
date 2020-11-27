3cc7ffe97a27a13643607bddd7692c50fc8c19be
meguia <eguiamc@gmail.com>
2020-11-27 07:55:45 -0300

    Soundscape using YAML and simplified

--- soundscape.py
+++ soundscape.py
@@ -2,25 +2,9 @@ import numpy as np
 from scipy import signal
 from scipy.interpolate import interp1d
 
-def load_pcm(file,nchan,nbytes=4):
-    """
-    Function to load a raw PCM audio file with nchan channels and nbytes little endian
-    """
-    data=np.memmap(file, dtype='u1', mode='r')
-    nsamples=data.shape[0]//(nchan*nbytes)
-    if nbytes==4:
-        realdata=np.reshape(data.view(np.int32),(nsamples,nchan))
-    elif nbytes==2:
-        realdata=np.reshape(data.view(np.int16),(nsamples,nchan))
-    elif nbytes==1:
-        realdata=np.reshape(data.view(np.int8),(nsamples,nchan))
-    else:
-        raise Exception("Only 4,2 or 1 bytes allowed")
-    return realdata
-
 def spectrogram(data, windowSize=512, overlap=None, fs=48000, windowType='hanning', normalized=False, logf=False):
     """
-    Computa el espetrograma de la senal data
+    Computa el espectrograma de la senal data
     devuelve spec un diccionario con keys 
     """
     #force to power of two
@@ -30,7 +14,7 @@ def spectrogram(data, windowSize=512, overlap=None, fs=48000, windowType='hannin
     if data.ndim == 1:
         data = data[:,np.newaxis] # el array debe ser 2D
     nsamples, nchan = np.shape(data)
-    nt = int(np.ceil((nsamples-windowSize)/(windowSize-overlap)))
+    nt = int(np.floor((nsamples-windowSize)/(windowSize-overlap)))+1
     # Dict for spectrogram
     listofkeys = ['nchan','f','t','s','nt','nf','df','window','type','overlap','log']
     spec = dict.fromkeys(listofkeys,0 )
@@ -70,78 +54,7 @@ def subspecs(spec,tstep,overlap=0.5):
     return subs.swapaxes(0,1),spec['t'][ts]
 
 
-def acoustic_complexity(spec,tstep):
-    subspec,t = subspecs(spec,tstep)
-    ACI = np.sum(np.sum(np.diff(subspec),axis=-1)/np.sum(subspec[0],axis=-1),axis=-1)
-    return t, ACI
-    
-    
-def bioacoustic_index(spec, tstep, frange=[2000,8000]):
-    if spec['logf']:
-        raise ValueError('NDSI compute for linear frequency only')
-    subspec,t = subspecs(spec,tstep)
-    f_bin = [int(np.around(a/spec['df'])) for a in frange]
-    spec_norm = subspec/np.amax(subspec,axis=(-2,-1))[...,np.newaxis,np.newaxis]
-    spec_mean = 10*np.log10(np.mean(np.square(spec_norm), axis=-1))
-    spec_mean_segment =  spec_mean[...,f_bin[0]:f_bin[1]]
-    spec_mean_segment_norm = spec_mean_segment - np.min(spec_mean_segment)
-    BI = np.sum(spec_mean_segment_norm/spec['df'],axis=-1)
-    return t,BI
-
-def spectral_entropy(spec, tstep):
-    """
-    Computes Spectral entropy of Shannon from spectrogram spect
-    """
-    subspec,t = subspecs(spec,tstep)
-    spec_av =  np.sum(subspec,axis=-1)
-    spec_av /= np.sum(spec_av,axis=-1)[...,np.newaxis]
-    HS = -np.sum(spec_av*np.log2(spec_av),axis=-1)/np.log2(spec['nf'])
-    return t,HS
-
-def ndsi(spec, tstep, anthrophony=[1000,2000], biophony=[2000,11000]):
-    """
-    Computes NDSI Using Spectrogram 
-    """
-    if spec['logf']:
-        raise ValueError('NDSI compute for linear frequency only')
-    subspec,t = subspecs(spec,tstep)
-    anthro_bin = [int(np.around(a/spec['df'])) for a in anthrophony]
-    bio_bin = [int(np.around(a/spec['df'])) for a in biophony]
-    anthro = np.sum(subspec[:,:,anthro_bin[0]:anthro_bin[1],:],axis=(-2,-1))
-    bio = np.sum(subspec[:,:,bio_bin[0]:bio_bin[1],:],axis=(-2,-1))
-    ndsi = (bio-anthro)/(bio+anthro)  
-    return t,ndsi
-    
-def gini(values,ax=0):
-    """
-    Compute the Gini index of values.
-    """
-    values.sort(axis=ax)
-    n = values.shape[ax]
-    idx = np.arange(1, n+1).reshape(-1,1)*np.ones_like(values)
-    G = np.sum(values*idx,axis=ax)
-    G = 2*G/np.sum(values,axis=ax) - (n+1)
-    return G/n
-
-def acoustic_diversity_even(spec, tstep, max_freq=10000, db_threshold = -50, freq_step=1000):
-    """
-    Compute the Acoustic Evenness Index AEI and Acoustic Diversity Index ADI
-    """
-    subspec,t = subspecs(spec,tstep)
-    bands_Hz = range(0, max_freq, freq_step)
-    bands_bin = [int(np.around(f / spec['df'])) for f in bands_Hz]
-    spec_AEI = 20*np.log10(subspec/np.amax(subspec,axis=(-2,-1))[...,np.newaxis,np.newaxis])
-    spec_AEI_bands = np.array([spec_AEI[:,:,bb:bb+bands_bin[1],:] for bb in bands_bin])
-    val = np.average(spec_AEI_bands>db_threshold,axis=(-2,-1)).swapaxes(0,1)
-    AEI = gini(val,ax=1)
-    tol = 1e-8
-    val[val<tol]=tol
-    ADI = np.sum(-val/np.sum(val,axis=(0,1))*np.log(val/np.sum(val,axis=(0,1))),axis=1)
-    return t, AEI, ADI       
-
-
-def indices(spec,tstep,ACI=True,BI=True,NDSI=True,AEI=True,ADI=True,HS=True,HT=True,H=True,
-            fbi=[2000,8000],fanthro=[1000,2000], fbio=[2000,11000],max_f=10000, db_thresh = -50, f_step=1000):
+def indices(spec,tstep,**kwargs):
     """
     Compute ALL indices
     """
@@ -150,34 +63,35 @@ def indices(spec,tstep,ACI=True,BI=True,NDSI=True,AEI=True,ADI=True,HS=True,HT=T
     subspec,t = subspecs(spec,tstep)
     ind['t']=t
     ind['nchan']=spec['nchan']
+    pars = kwargs['Parameters']
     spec_norm = subspec/np.amax(subspec,axis=(-2,-1))[...,np.newaxis,np.newaxis]
-    if ACI:
+    if 'ACI' in pars:
         ind['aci'] = np.sum(np.sum(np.diff(subspec),axis=-1)/np.sum(subspec[0],axis=-1),axis=-1)
-    if BI:
-        f_bin = [int(np.around(a/spec['df'])) for a in fbi]
+    if 'BI' in pars:
+        f_bin = [int(np.around(a/spec['df'])) for a in kwargs['freq_BI']]
         spec_mean = 10*np.log10(np.mean(np.square(spec_norm), axis=-1))
         spec_mean_segment =  spec_mean[...,f_bin[0]:f_bin[1]]
         spec_mean_segment_norm = spec_mean_segment - np.min(spec_mean_segment)
         ind['bi'] = np.sum(spec_mean_segment_norm/spec['df'],axis=-1) 
-    if NDSI:
-        anthro_bin = [int(np.around(a/spec['df'])) for a in fanthro]
-        bio_bin = [int(np.around(a/spec['df'])) for a in fbio]
+    if 'NDSI' in pars:
+        anthro_bin = [int(np.around(a/spec['df'])) for a in kwargs['freq_anthro']]
+        bio_bin = [int(np.around(a/spec['df'])) for a in kwargs['freq_bio']]
         anthro = np.sum(subspec[:,:,anthro_bin[0]:anthro_bin[1],:],axis=(-2,-1))
         bio = np.sum(subspec[:,:,bio_bin[0]:bio_bin[1],:],axis=(-2,-1))
         ind['ndsi'] = (bio-anthro)/(bio+anthro)
-    if HS:
+    if 'HS' in pars:
         spec_av =  np.sum(subspec,axis=-1)
         spec_av /= np.sum(spec_av,axis=-1)[...,np.newaxis]
         ind['hs'] = -np.sum(spec_av*np.log2(spec_av),axis=-1)/np.log2(spec['nf'])    
-    if AEI or ADI:
-        bands_Hz = range(0, max_f, f_step)
+    if 'AEI' in pars or 'ADI' in pars:
+        bands_Hz = range(0, kwargs['max_freq'], kwargs['freq_step'])
         bands_bin = [int(np.around(f / spec['df'])) for f in bands_Hz]
         spec_AEI = 20*np.log10(spec_norm)
         spec_AEI_bands = np.array([spec_AEI[:,:,bb:bb+bands_bin[1],:] for bb in bands_bin])
-        val = np.average(spec_AEI_bands>db_thresh,axis=(-2,-1)).swapaxes(0,1)
-        if AEI:
+        val = np.average(spec_AEI_bands>kwargs['db_threshold'],axis=(-2,-1)).swapaxes(0,1)
+        if 'AEI' in pars:
             ind['aei']=gini(val,ax=1)
-        if ADI:
+        if 'ADI' in pars:
             tol = 1e-8
             val[val<tol]=tol
             ind['adi'] = np.sum(-val/np.sum(val,axis=(0,1))*np.log(val/np.sum(val,axis=(0,1))),axis=1)
